07c14a117ff4589c38686ca1504d2e8191bdb600
nandhuknair <nandhuknair27@gmail.com>
2024-10-04 08:01:42 +0530

    changed middleware

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,6 @@ const userDetails = require('../controller/userDetails')
 const logout = require('../controller/logout')
 const updateUserDetails = require('../controller/updateUserDetails')
 const searchUser = require('../controller/searchUser')
-
-const authMiddleware = require('../middleware/authMiddleware');
-
 const router = express.Router()
 
 router.post('/register',registerUser)
@@ -16,8 +13,8 @@ router.post('/email',checkEmail)
 router.post('/password',checkPassword)
 router.get('/user-details',userDetails)
 router.get('/logout',logout)
-router.post('/update-user',authMiddleware,updateUserDetails)
-router.post('/search-user',authMiddleware,searchUser)
+router.post('/update-user',updateUserDetails)
+router.post('/search-user',searchUser)
 
 
 
