48cd89d48399f60a1cd2dbefc8ed7b506ed00010
nandhuknair <nandhuknair27@gmail.com>
2024-10-04 12:24:00 +0530

    updated the userdetail console 22

--- controller/userDetails.js
+++ controller/userDetails.js
@@ -2,10 +2,18 @@ const getUserDetailsFromToken = require("../helpers/getUserDetailsFromToken");
 
 async function userDetails(request, response) {
   try {
-    const token = request.cookies.token || "";
-    console.log(token,
-      "!!!!!!!!!This is the token from the cookie of getUserDetails page"
-    );
+    const token =
+      request.cookies.token || request.headers.authorization?.split(' ')[1] || "";
+
+    if (!token) {
+      return response.status(401).json({
+        message: "No token provided",
+        logout: true,
+      });
+    }
+
+    console.log("Token received:", token);
+
     const user = await getUserDetailsFromToken(token);
 
     if (user.logout) {
