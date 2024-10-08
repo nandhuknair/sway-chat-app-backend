70894dbeb3814bb9bce0cf44a061d8b787335328
nandhuknair <nandhuknair27@gmail.com>
2024-10-08 22:17:22 +0530

    updated the register controller

--- controller/registerUser.js
+++ controller/registerUser.js
@@ -9,7 +9,7 @@ async function registerUser(request,response){
 
         if(checkEmail){
             return response.status(400).json({
-                message : "Already user exits",
+                message : "Already user exit",
                 error : true,
             })
         }
